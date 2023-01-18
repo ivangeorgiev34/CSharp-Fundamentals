10630d2ea750fd71e4e54b7275e1ba00eb2654b4
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-18 20:56:42 +0200

    Added exercise 10.MultiplicationTable

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/10.MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter <= 10)
+            {
+                Console.WriteLine($"{number} X {counter} = {number * counter}");
+                counter++;
+            }
+        }
+    }
+}
