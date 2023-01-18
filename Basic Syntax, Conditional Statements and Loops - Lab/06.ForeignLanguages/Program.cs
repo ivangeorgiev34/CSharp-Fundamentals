4f015ccb53c9bbce60ed3ae6aef393790c6892b3
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-18 20:55:11 +0200

    Added exercise 06.ForeignLanguages

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/06.ForeignLanguages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06.ForeignLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                    Console.WriteLine("Spanish");
+                    break;
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                case "Argentina":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+    }
+}
