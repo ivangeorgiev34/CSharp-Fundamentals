2eb64cdd8487e8727b0b02557beccbb27b1507e5
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-18 20:56:15 +0200

    Added exercise 09.SumOfOddNumbers

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int sum = 0;
+            for (int i = 1; i < 100; i++)
+            {
+                if (!(i % 2 == 0))
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                    counter++;
+                }
+                if (n == counter)
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
