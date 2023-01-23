9f0762f39cbd5b0deff0766138ab0fa857e8af8b
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-23 22:48:46 +0200

    Added exercise 04.ReverseString

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/04.ReverseString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            List<char> chars = str.ToCharArray().Reverse().ToList();
+
+
+            Console.WriteLine(string.Join("",chars));
+        }
+    }
+}
