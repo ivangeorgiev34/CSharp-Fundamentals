dd5092f69186d705bf4291fc87d3da15babead4f
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-23 22:47:22 +0200

    Added exercise 01.SortNumbers

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/01.SortNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double thirdNumber = double.Parse(Console.ReadLine());
+
+            List<double> numbers = new List<double>();
+
+            numbers.Add(firstNumber);
+            numbers.Add(secondNumber);
+            numbers.Add(thirdNumber);
+
+            foreach (var number in numbers.OrderByDescending(n => n))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
