52a337dfbd9c693da20bccc67f0ff739c684d8d7
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-24 16:35:53 +0200

    Added exercise 03.ExactSumOfRealNumbers

---
+++ Data Types and Variables - Lab/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= countOfNumbers; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum = sum + number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
