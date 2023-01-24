f34238f0e34778216d0d73bb9522cf9956f33f3e
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-24 16:34:39 +0200

    Added exercise 01.ConvertMetersToKilometers

---
+++ Data Types and Variables - Lab/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometers = meters / 1000.0;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
