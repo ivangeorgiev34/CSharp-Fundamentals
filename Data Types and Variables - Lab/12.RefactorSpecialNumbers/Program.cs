7939091b81f7dd92b0e6baddda97199dd098c658
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-24 16:45:35 +0200

    Added exercise 12.RefactorSpecialNumbers

---
+++ Data Types and Variables - Lab/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numbers; i++)
+            {
+                int sumOfDigits = 0;
+                int currentDigit = i;
+                bool isSumValid = false;
+                while (currentDigit != 0)
+                {
+                    sumOfDigits += currentDigit % 10;
+                    currentDigit = currentDigit / 10;
+                }
+
+                isSumValid = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine($"{i} -> {isSumValid}");
+
+            }
+        }
+    }
+}
