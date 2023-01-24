d3048f84440876a2f42caf345ade17882023d150
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-24 16:36:49 +0200

    Added exercise 05.SpecialNumbers

---
+++ Data Types and Variables - Lab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= num; i++)
+            {
+                int sumOfDigits = 0;
+                int currentDigit = i;
+                while (currentDigit != 0)
+                {
+                    sumOfDigits = sumOfDigits + currentDigit % 10;
+                    currentDigit = currentDigit / 10;
+                }
+
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
