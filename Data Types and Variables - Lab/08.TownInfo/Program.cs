bfc213447d6734d8d8c6b00cb941384d4df4f265
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-24 16:41:51 +0200

    Added exercise 08.TownInfo

---
+++ Data Types and Variables - Lab/08.TownInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08.TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfTown = Console.ReadLine();
+            ulong population = ulong.Parse(Console.ReadLine());
+            uint kilometers = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {nameOfTown} has population of {population} and area {kilometers} square km.");
+        }
+    }
+}
