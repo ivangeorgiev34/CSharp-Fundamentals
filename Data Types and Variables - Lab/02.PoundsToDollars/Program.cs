c8ae7b8819c1baeb9bbf90a120bdc229c1b0da56
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-24 16:35:09 +0200

    Added exercise 02.PoundsToDollars

---
+++ Data Types and Variables - Lab/02.PoundsToDollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double britishPounds = double.Parse(Console.ReadLine());
+            double americanDollars = 1.31 * britishPounds;
+            Console.WriteLine($"{americanDollars:f3}");
+        }
+    }
+}
