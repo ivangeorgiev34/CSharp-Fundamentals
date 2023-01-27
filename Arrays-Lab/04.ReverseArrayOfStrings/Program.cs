77396c6620aa273feb4f6ebf3cea5abfed7c981b
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-27 16:13:53 +0200

    Added exercise 04.ReverseArrayOfStrings

---
+++ Arrays-Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ').ToArray();
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
