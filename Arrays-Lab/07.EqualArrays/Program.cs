d5f6ac03a3c40921399236b9556742674a244f9a
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-27 16:14:52 +0200

    Added exercise 07.EqualArrays

---
+++ Arrays-Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = 0;
+            int errorIndex = 0;
+            int counter = 0;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] == secondArray[i])
+                {
+                    counter++;
+                    sum = sum + firstArray[i];
+                }
+                else
+                {
+                    errorIndex = i;
+                    break;
+                }
+            }
+            if (counter == firstArray.Length)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {errorIndex} index");
+            }
+        }
+    }
+}
