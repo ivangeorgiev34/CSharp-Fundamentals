815bc76280ec5d9356446a051a80bbcc79806d8e
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-27 16:13:25 +0200

    Added exercise 03.RoundingNumbers

---
+++ Arrays-Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbersArray = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                Console.WriteLine($"{numbersArray[i]} => {(int)Math.Round(numbersArray[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
