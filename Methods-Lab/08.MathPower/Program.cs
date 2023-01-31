462cb56fba4be1a6d9db5b9eff4f8ea61ca71d78
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:36:20 +0200

    Added exercise 08.MathPower

---
+++ Methods-Lab/08.MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            double powerNumber = double.Parse(Console.ReadLine());
+            double result = PoweredNumber(baseNumber, powerNumber);
+            Console.WriteLine(result);
+        }
+
+        static double PoweredNumber(double baseNumber, double powerNumber)
+        {
+            return Math.Pow(baseNumber, powerNumber);
+        }
+    }
+}
