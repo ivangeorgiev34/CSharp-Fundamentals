06e1ef395a0fe3756959892df068b6418155dd79
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:34:17 +0200

    Added exercise 03.Calculations

---
+++ Methods-Lab/03.Calculations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string operation = Console.ReadLine();
+                double firstNumber = int.Parse(Console.ReadLine());
+                double secondNumber = int.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case "add":
+                        AddNumbers(firstNumber, secondNumber);
+                        break;
+                    case "multiply":
+                        MultiplyNumbers(firstNumber, secondNumber);
+                        break;
+                    case "substract":
+                        SubstractNumbers(firstNumber, secondNumber);
+                        break;
+                    case "divide":
+                        DivideNumbers(firstNumber, secondNumber);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            static void AddNumbers(double firstNumber, double secondNumber)
+            {
+                double result = firstNumber + secondNumber;
+                Console.WriteLine(result);
+            }
+            static void MultiplyNumbers(double firstNumber, double secondNumber)
+            {
+                double result = firstNumber * secondNumber;
+                Console.WriteLine(result);
+            }
+            static void SubstractNumbers(double firstNumber, double secondNumber)
+            {
+                double result = firstNumber - secondNumber;
+                Console.WriteLine(result);
+            }
+            static void DivideNumbers(double firstNumber, double secondNumber)
+            {
+                double result = firstNumber / secondNumber;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
