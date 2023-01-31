a78774e719579f57654f8066a28711b3c004c392
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:34:54 +0200

    Added exercise 04.PrintingTriangle

---
+++ Methods-Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trianglePyramidLength = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= trianglePyramidLength; i++)
+            {
+                PrintTrianglePyramid(1, i);
+            }
+            for (int i = trianglePyramidLength - 1; i >= 1; i--)
+            {
+                PrintTrianglePyramid(1, i);
+            }
+        }
+
+        static void PrintTrianglePyramid(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
