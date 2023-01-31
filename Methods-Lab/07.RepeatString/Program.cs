8d24b10d00c1f342c59cf4b68e9efe0b0e8d4853
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:36:00 +0200

    Added exercise 07.RepeatString

---
+++ Methods-Lab/07.RepeatString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace _07.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int timesRepeatedText = int.Parse(Console.ReadLine());
+            string result = RepeatedString(text, timesRepeatedText);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatedString(string text, int timesRepeatedText)
+        {
+            StringBuilder repeatedString = new StringBuilder();
+            for (int i = 0; i < timesRepeatedText; i++)
+            {
+                repeatedString.Append(text);
+            }
+            return repeatedString.ToString();
+        }
+    }
+}
