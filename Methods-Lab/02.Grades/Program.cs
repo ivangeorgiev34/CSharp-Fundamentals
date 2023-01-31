1759b4f70c72ad17d42aab56fec8d904655b8d25
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:33:54 +0200

    Added exercise 02.Grades

---
+++ Methods-Lab/02.Grades/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02.Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            GradeDefinition(grade);
+
+        }
+
+        static void GradeDefinition(double grade)
+        {
+            //•	2.00 – 2.99 - "Fail"
+            //•	3.00 – 3.49 - "Poor"
+            //•	3.50 – 4.49 - "Good"
+            //•	4.50 – 5.49 - "Very good"
+            //•	5.50 – 6.00 - "Excellent"
+
+            if (grade >= 2 && grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.50 && grade <= 6)
+            {
+                Console.WriteLine("Excellent");
+            }
+
+
+
+        }
+    }
+}
