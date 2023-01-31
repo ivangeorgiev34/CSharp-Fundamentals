9bd8de368adc46cc5127cd91b801c3a090be74f4
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:37:18 +0200

    Added exercise 10.MultiplyEvensByOdds

---
+++ Methods-Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _10.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                int number = int.Parse(Console.ReadLine());
+                number = Math.Abs(number);
+                Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+            }
+
+            static int GetSumOfEvenDigits(int number)
+            {
+                int sumEven = 0;
+
+                while (number > 0)
+                {
+                    int currentDigit = number % 10;
+                    if (currentDigit % 2 == 0)
+                    {
+                        sumEven = sumEven + currentDigit;
+
+                    }
+                    number = number / 10;
+                }
+                return sumEven;
+            }
+            static int GetSumOfOddDigits(int number)
+            {
+                int sumOdd = 0;
+
+                while (number > 0)
+                {
+                    int currentDigit = number % 10;
+                    if (!(currentDigit % 2 == 0))
+                    {
+                        sumOdd = sumOdd + currentDigit;
+
+                    }
+                    number = number / 10;
+                }
+                return sumOdd;
+            }
+            static int GetMultipleOfEvenAndOdds(int number)
+            {
+                int evenSum = GetSumOfEvenDigits(number);
+                int oddSum = GetSumOfOddDigits(number);
+                return evenSum * oddSum;
+
+            }
+        }
+    }
+}
