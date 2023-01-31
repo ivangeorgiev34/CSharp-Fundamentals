e3935480b1f2ca407210e26e5053625d067e47e3
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:35:19 +0200

    Added exercise 05.Orders

---
+++ Methods-Lab/05.Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double price = 0;
+            switch (product)
+            {
+                case "coffee":
+                    price = 1.5;
+                    break;
+                case "water":
+                    price = 1;
+                    break;
+                case "coke":
+                    price = 1.4;
+                    break;
+                case "snacks":
+                    price = 2;
+                    break;
+                default:
+                    break;
+            }
+            PrintPriceOfOrder(quantity, price);
+        }
+
+        static void PrintPriceOfOrder(int quantity, double price)
+        {
+            Console.WriteLine($"{(quantity * price):f2}");
+        }
+    }
+}
