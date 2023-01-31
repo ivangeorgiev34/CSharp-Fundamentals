1d226fd0c4e4621879a341932bdbb4e28993e63b
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:35:39 +0200

    Added exercise 06.CalculateRectangleArea

---
+++ Methods-Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideOne = int.Parse(Console.ReadLine());
+            int sideTwo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RectangleArea(sideOne, sideTwo));
+        }
+
+        static int RectangleArea(int sideOne, int sideTwo)
+        {
+            int area = sideOne * sideTwo;
+            return area;
+        }
+    }
+}
