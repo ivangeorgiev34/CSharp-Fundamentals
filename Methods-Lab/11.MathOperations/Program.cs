dd2287a3430759152c2859d307071c43f61413ce
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-01-31 15:37:41 +0200

    Added exercise 11.MathOperations

---
+++ Methods-Lab/11.MathOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            if (@operator == '/')
+            {
+                Console.WriteLine(DivideNumbers(firstNumber, secondNumber));
+            }
+            else if (@operator == '*')
+            {
+                Console.WriteLine(MultiplyNumbers(firstNumber, secondNumber));
+            }
+            else if (@operator == '-')
+            {
+                Console.WriteLine(SubstractNumbers(firstNumber, secondNumber));
+            }
+            else if (@operator == '+')
+            {
+                Console.WriteLine(AddNumbers(firstNumber, secondNumber));
+            }
+        }
+        static int MultiplyNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        static int DivideNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+        static int SubstractNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        static int AddNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+
+    }
+}
+
