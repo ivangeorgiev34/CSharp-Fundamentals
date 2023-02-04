2d9b87e8407c75b6cb2938cc10df77726fdf59f4
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-04 19:48:32 +0200

    Added exercise 03.MergingLists

---
+++ Lists-Lab/03.MergingLists/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.MergingLists
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+			List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+			List<int> mergedList = new List<int>();
+			int biggerList = Math.Max(firstList.Count, secondList.Count);
+			for (int i = 0; i < biggerList; i++)
+			{
+				if (firstList.Count > i)
+				{
+					mergedList.Add(firstList[i]);
+				}
+				if (secondList.Count > i)
+				{
+					mergedList.Add(secondList[i]);
+				}
+			}
+			Console.WriteLine(string.Join(" ", mergedList));
+		}
+	}
+}
