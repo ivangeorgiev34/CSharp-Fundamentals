461fbf4c3e6c40131b49d1f7491c59eeb25349df
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-04 19:48:55 +0200

    Added exercise 04.ListOfProducts

---
+++ Lists-Lab/04.ListOfProducts/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.ListOfProducts
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> list = new List<string>();
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++) 
+			{
+				string product = Console.ReadLine();
+				list.Add(product);
+			}
+			list.Sort();
+			for (int i = 0; i < list.Count; i++)
+			{
+				Console.WriteLine($"{i + 1}.{list[i]}");
+			}
+		}
+	}
+}
