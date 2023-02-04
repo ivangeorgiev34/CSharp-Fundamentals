920879a3130ac127c69573bfcf50f3db98c482cb
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-04 19:46:48 +0200

    Added exercise 01.SumAdjacentEqualNumbers

---
+++ Lists-Lab/01.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.SumAdjacentEqualNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<double> numberInput = Console.ReadLine().Split().Select(double.Parse).ToList();
+			int listCount = numberInput.Count;
+			for (int i = 0; i < numberInput.Count - 1; i++)
+			{
+				if (numberInput[i] == numberInput[i + 1])
+				{
+					numberInput[i] = numberInput[i] + numberInput[i + 1];
+					numberInput.RemoveAt(i + 1);
+					i = -1;
+				}
+			}
+			Console.WriteLine(string.Join(" ", numberInput));
+		}
+	}
+}
