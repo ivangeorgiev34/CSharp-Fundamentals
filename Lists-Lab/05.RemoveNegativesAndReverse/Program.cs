f492d313c0dbe95398e14bf55dc63b7bdbcb316c
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-04 19:49:12 +0200

    Added exercise 05.RemoveNegativesAndReverse

---
+++ Lists-Lab/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.RemoveNegativesAndReverse
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> listOfNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+			int listCount = listOfNumbers.Count;
+			for (int i = 0; i < listOfNumbers.Count; i++) // 7 -2 -10 1
+			{
+				if (listOfNumbers[i] < 0)
+				{
+					listOfNumbers.RemoveAt(i);
+					i--;
+				}
+			}
+			listOfNumbers.Reverse();
+			if (listOfNumbers.Count == 0)
+			{
+				Console.WriteLine("empty");
+			}
+			else
+			{
+				Console.WriteLine(string.Join(" ", listOfNumbers));
+			}
+		}
+	}
+}
