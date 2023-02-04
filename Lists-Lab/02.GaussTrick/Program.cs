87425c87ea3fdab33b0a0df50a245c2972947b98
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-04 19:47:59 +0200

    Added exercise 02.GaussTrick

---
+++ Lists-Lab/02.GaussTrick/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.GaussTrick
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+			List<int> result = new List<int>();
+
+			for (int i = 0; i < numbers.Count / 2; i++)
+			{
+				result.Add(numbers[i] + numbers[numbers.Count - 1 - i]); 
+
+			}
+			if (numbers.Count % 2 != 0)
+			{
+				result.Add(numbers[numbers.Count / 2]);
+			}
+			Console.WriteLine(string.Join(" ", result));
+		}
+	}
+}
