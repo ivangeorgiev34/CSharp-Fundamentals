dd54b5308b10957526e73bad1e11f6c9bb4be57a
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-06 12:02:04 +0200

    Added exercise 01.RandomizeWords

---
+++ ObjectsAndClasses-Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Random number = new Random();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int randomIndex = number.Next(0, input.Length);
+
+                string temp = input[i];
+                input[i] = input[randomIndex];
+                input[randomIndex] = temp;
+
+            }
+            foreach (var word in input)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
