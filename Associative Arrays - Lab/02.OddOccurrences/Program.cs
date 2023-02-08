9564062520d4568eff5faa9f359126ab2490e9ce
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-08 12:09:41 +0200

    Added exercise 02.OddOccurrences

---
+++ Associative Arrays - Lab/02.OddOccurrences/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            string[] array = Console.ReadLine().Split().ToArray();
+            foreach (var word in array)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (!dictionary.ContainsKey(wordInLowerCase))
+                {
+                    dictionary.Add(wordInLowerCase, 1);
+                }
+                else
+                {
+                    dictionary[wordInLowerCase]++;
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                if (!(item.Value % 2 == 0))
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+        }
+    }
+}
