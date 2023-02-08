2c4a79b5218e5749054e46a4e9f25d7a1fdfae42
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-08 12:09:10 +0200

    Added exercise 01.CountRealNumbers

---
+++ Associative Arrays - Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, int> dictionary = new SortedDictionary<int, int>();
+            int[] arr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            foreach (int number in arr)
+            {
+                if (!dictionary.ContainsKey(number))
+                {
+                    dictionary.Add(number, 1);
+                }
+                else
+                {
+                    dictionary[number]++;
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
