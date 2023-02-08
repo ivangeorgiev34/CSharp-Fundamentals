339d7f5ef08bcf754c6bf4d9fd0ae87e19db1489
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-08 12:10:04 +0200

    Added exercise 03.WordSynonyms

---
+++ Associative Arrays - Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary.Add(word, new List<string>());
+                }
+
+                dictionary[word].Add(synonym);
+
+            }
+            foreach (var word in dictionary)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
