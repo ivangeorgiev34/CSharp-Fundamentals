ce620bf3257603cb2c19288c59a211fc81b6479f
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-08 12:10:21 +0200

    Added exercise 04.WordFilter

---
+++ Associative Arrays - Lab/04.WordFilter/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Where(w => w.Length % 2 == 0).Select(w => w).ToArray();
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
