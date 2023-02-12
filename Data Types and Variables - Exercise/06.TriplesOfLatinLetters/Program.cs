82954dad2c7a7e1f6015d79d5cffa9e3736476bb
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-12 16:54:32 +0200

    Added exercise 06.TriplesOfLatinLetters

---
+++ Data Types and Variables - Exercise/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.TriplesOfLatinLetters
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int STARTOFALPHABET = 97;
+
+
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					for (int k = 0; k < n; k++)
+					{
+						Console.WriteLine($"{(char)(STARTOFALPHABET + i)}{(char)(STARTOFALPHABET + j)}{(char)(STARTOFALPHABET + k)}");
+					}
+				}
+			}
+		}
+	}
+}
