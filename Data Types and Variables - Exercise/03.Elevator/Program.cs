2d2cbfe0d3c3bba05c151fd0d2fd1e42eeab7336
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-12 16:53:01 +0200

    Added exercise 03.Elevator

---
+++ Data Types and Variables - Exercise/03.Elevator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _03.Elevator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double totalPeople = double.Parse(Console.ReadLine());
+			double capacity = double.Parse(Console.ReadLine());
+			Console.WriteLine(Math.Ceiling(totalPeople / capacity));
+		}
+	}
+}
