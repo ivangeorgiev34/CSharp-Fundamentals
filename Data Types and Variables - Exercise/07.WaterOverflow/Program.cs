f4354df1682a00cd4b2efb2de10b63a97f8bd7ec
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-12 16:55:09 +0200

    Added exercise 07.WaterOverflow

---
+++ Data Types and Variables - Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.WaterOverflow
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int WATERTANKCAPACITY = 255;
+			int n = int.Parse(Console.ReadLine());
+			int litersSum = 0;
+			for (int i = 0; i < n; i++)
+			{
+				int quantity = int.Parse(Console.ReadLine());
+				litersSum = litersSum + quantity;
+				if (WATERTANKCAPACITY < litersSum)
+				{
+					Console.WriteLine($"Insufficient capacity!");
+					litersSum = litersSum - quantity;
+				}
+			}
+			Console.WriteLine(litersSum);
+		}
+	}
+}
