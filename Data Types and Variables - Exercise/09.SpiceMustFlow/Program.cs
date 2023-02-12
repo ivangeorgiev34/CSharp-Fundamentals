67e554727d1e61d8c6b499bd4c213ccce3621101
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-12 16:56:31 +0200

    Added exercise 09.SpiceMustFlow

---
+++ Data Types and Variables - Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _09.SpiceMustFlow
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int WORKERS_CONSUMPTION = 26;
+			const int DAILYDECREMENTATION = 10;
+			int startingYield = int.Parse(Console.ReadLine());
+			int daysCounter = 0;
+			int totalSum = 0;
+			while (startingYield >= 100)
+			{
+				daysCounter++;
+				totalSum = totalSum + startingYield - WORKERS_CONSUMPTION;
+				startingYield = startingYield - DAILYDECREMENTATION;
+				if (startingYield < 100)
+				{
+					totalSum = totalSum - WORKERS_CONSUMPTION;
+				}
+			}
+			Console.WriteLine(daysCounter);
+			Console.WriteLine(totalSum);
+		}
+	}
+}
