8eefa5c1c5f43014a97649dc66b270b8a210d9ee
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-12 16:57:12 +0200

    Added exercise 10.PokeMon

---
+++ Data Types and Variables - Exercise/10.PokeMon/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _10.PokeMon
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int m = int.Parse(Console.ReadLine());
+			int y = int.Parse(Console.ReadLine());
+			int pokedTargets = 0;
+			int originalValueOfN = n;
+			while (n >= m)
+			{
+				n = n - m;
+				pokedTargets++;
+				if (n * 2 == originalValueOfN && y > 0)
+				{
+					n = n / y;
+				}
+			}
+
+			Console.WriteLine(n);
+			Console.WriteLine(pokedTargets);
+		}
+	}
+}
