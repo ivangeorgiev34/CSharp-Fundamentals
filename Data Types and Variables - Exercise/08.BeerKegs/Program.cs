9c890c96c974a0e84a07bff0acf858b539798431
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-12 16:55:52 +0200

    Added exercise 08.BeerKegs

---
+++ Data Types and Variables - Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08.BeerKegs
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			string biggestKegModel = null;
+			double biggestVolume = 0;
+			for (int i = 0; i < n; i++)
+			{
+				string model = Console.ReadLine();
+				double radius = double.Parse(Console.ReadLine());
+				int height = int.Parse(Console.ReadLine());
+				double volume = Math.PI * radius * radius * height;
+				if (volume > biggestVolume)
+				{
+					biggestVolume = volume;
+					biggestKegModel = model;
+				}
+			}
+			Console.WriteLine(biggestKegModel);
+		}
+	}
+}
