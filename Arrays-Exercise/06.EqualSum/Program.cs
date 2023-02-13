c555adb53da44f351eff5897b1c298d7b9774a1e
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:17:45 +0200

    Added exercise 06.EqualSum

---
+++ Arrays-Exercise/06.EqualSum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06.EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int leftSideSum = 0;
+            int rightSideSum = 0;
+            for (int indexOne = 0; indexOne < array.Length; indexOne++) // 1 2 3 3
+            {
+                if (array.Length == 1)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+                leftSideSum = 0;
+                for (int leftSide = 0; leftSide < indexOne; leftSide++)
+                {
+                    leftSideSum = leftSideSum + array[leftSide];
+                }
+
+                rightSideSum = 0;
+                for (int rightSide = array.Length - 1; indexOne < rightSide; rightSide--)
+                {
+                    rightSideSum = rightSideSum + array[rightSide];
+                }
+
+                if (leftSideSum == rightSideSum)
+                {
+                    Console.WriteLine(indexOne);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
