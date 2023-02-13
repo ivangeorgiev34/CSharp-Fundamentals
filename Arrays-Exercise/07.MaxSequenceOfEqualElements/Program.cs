82289d6f6705e0fc1d6a6b69f3685b043a054e8d
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:18:03 +0200

    Added exercise 07.MaxSequenceOfEqualElements

---
+++ Arrays-Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int longestStreakCount = 0;
+            int currentStreakCount = 1;
+            string longestStreak = string.Empty;
+            string currentStreak = string.Empty;
+            for (int i = 0; i < numbers.Length - 1; i++) //   2 1 1 2 3 3 2 2 2 1
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    if (currentStreakCount == 1)
+                    {
+                        currentStreak = numbers[i].ToString() + " ";
+                    }
+                    currentStreakCount++;
+                    currentStreak = currentStreak + numbers[i] + " ";
+                }
+                else
+                {
+                    currentStreakCount = 1;
+                    currentStreak = numbers[i].ToString() + " ";
+                }
+                if (currentStreakCount > longestStreakCount)
+                {
+                    longestStreak = currentStreak;
+                    longestStreakCount = currentStreakCount;
+                }
+            }
+            Console.WriteLine(longestStreak);
+        }
+    }
+}
