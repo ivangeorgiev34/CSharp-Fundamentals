634009483b6e48a79537d335ca0df79aa8503201
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:16:33 +0200

    Added exercise 02.CommonElements

---
+++ Arrays-Exercise/02.CommonElements/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrOne = Console.ReadLine().Split();
+            string[] arrTwo = Console.ReadLine().Split();
+
+            for (int i = 0; i < arrOne.Length; i++)
+            {
+                for (int j = 0; j < arrTwo.Length; j++)
+                {
+
+                    if (arrTwo[j] == arrOne[i])
+                    {
+                        Console.Write($"{arrTwo[j]} ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
