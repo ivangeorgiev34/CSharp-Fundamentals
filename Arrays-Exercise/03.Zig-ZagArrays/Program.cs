39fafe16248dd35b5bb6cfd4f8d1bafdbf9b0925
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:16:49 +0200

    Added exercise 03.Zig-ZagArrays

---
+++ Arrays-Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] evenArray = new int[n];
+            int[] oddArray = new int[n];
+            for (int i = 0; i < evenArray.Length; i++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    evenArray[i] = numbers[0];
+                    oddArray[i] = numbers[1];
+                }
+                else
+                {
+                    evenArray[i] = numbers[1];
+                    oddArray[i] = numbers[0];
+                }
+            }
+            Console.WriteLine(string.Join(" ", evenArray));
+            Console.WriteLine(string.Join(" ", oddArray));
+        }
+    }
+}
