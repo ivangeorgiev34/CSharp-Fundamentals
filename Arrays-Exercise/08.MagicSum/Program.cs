7ad8e8a0086888b5ae0a1e8ee92255eac96bb0af
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:18:21 +0200

    Added exercise 08.MagicSum

---
+++ Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicNumber = int.Parse(Console.ReadLine());
+            for (int indexOne = 0; indexOne < array.Length; indexOne++)       // 1 7 6 2 19 23
+            {
+                for (int indexTwo = indexOne + 1; indexTwo < array.Length; indexTwo++)
+                {
+                    if (array[indexOne] + array[indexTwo] == magicNumber)
+                    {
+                        Console.WriteLine(string.Join(" ", array[indexOne], array[indexTwo]));
+
+                    }
+                }
+            }
+        }
+    }
+    }
+}
