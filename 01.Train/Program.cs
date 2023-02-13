c5865710b8e7892e08100c1c2f78bd761344250a
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:15:05 +0200

    Added exercise 01.Train

---
+++ 01.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                sum = sum + arr[i];
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
