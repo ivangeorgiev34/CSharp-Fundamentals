0382d4b8f678a375350aa1bc1bbaf84a215e5c78
ivangeorgiev34 <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-13 12:17:29 +0200

    Added exercise 05.TopIntegers

---
+++ Arrays-Exercise/05.TopIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int indexOne = 0; indexOne < array.Length; indexOne++) // 1 4 3 2
+            {
+                int currentNumber = array[indexOne];
+                bool isNumberGreater = true;
+                for (int indexTwo = indexOne + 1; indexTwo < array.Length; indexTwo++)
+                {
+
+                    int nextNumber = array[indexTwo];
+                    if (currentNumber <= nextNumber)
+                    {
+                        isNumberGreater = false;
+                        break;
+                    }
+                }
+                if (isNumberGreater)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
