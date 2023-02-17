1685000dae9b0294cc309a5f0bef7fec73f9bdad
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:58:27 +0200

    Add exercise 09.PalindromeIntegers

---
+++ Methods - Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+
+                Console.WriteLine(CheckNumberIfPalindrome(command).ToString().ToLower());
+                command = Console.ReadLine();
+
+            }
+        }
+
+        static bool CheckNumberIfPalindrome(string command)
+        {
+            if (command[0] == command[command.Length - 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
