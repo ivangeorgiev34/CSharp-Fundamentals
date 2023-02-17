74db273ee9f49805767613d8379da8c49024b679
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:56:39 +0200

    Add exercis e

---
+++ Methods - Exercise/05.AddAndSubtract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05.AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(SubstractSum(firstNumber, secondNumber, thirdNumber));
+        }
+        static int AddNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        static int SubstractSum(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return AddNumbers(firstNumber, secondNumber) - thirdNumber;
+        }
+    }
+}
