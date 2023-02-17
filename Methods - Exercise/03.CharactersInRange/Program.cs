8a12624ffc263a92ed6fe6806449ca6144adc4a4
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:55:31 +0200

    Add exercise 03.CharactersInRange

---
+++ Methods - Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            PrintChars(firstChar, secondChar);
+        }
+        static void PrintChars(char firstChar, char secondChar)
+        {
+            int biggerChar = Math.Max(firstChar, secondChar);
+            int smallerChar = Math.Min(firstChar, secondChar);
+            for (int i = smallerChar + 1; i < biggerChar; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
