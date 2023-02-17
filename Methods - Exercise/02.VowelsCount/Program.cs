b967ee2cf1dcb651bce93ea099a5461fdb2b5459
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:55:07 +0200

    Add exercise 02.VowelsCount

---
+++ Methods - Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            VowelsInInput(input);
+        }
+        static void VowelsInInput(string input)
+        {
+            int counter = 0;
+            string vowels = "aouei";
+            for (int i = 0; i < input.Length; i++) // softuni
+            {
+                if (input[i] == 'a')
+                {
+                    counter++;
+                }
+                else if (input[i] == 'o')
+                {
+                    counter++;
+                }
+                else if (input[i] == 'u')
+                {
+                    counter++;
+                }
+                else if (input[i] == 'e')
+                {
+                    counter++;
+                }
+                else if (input[i] == 'i')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
