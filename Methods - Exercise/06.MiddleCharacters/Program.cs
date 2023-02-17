8f152bef3c0e2608a004036cd64b751a5d84c196
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:57:26 +0200

    Add exercise 06.MiddleCharacters

---
+++ Methods - Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleCharacters(input);
+        }
+        static void PrintMiddleCharacters(string input)
+        {
+            if (input.Length % 2 == 0) // 6
+            {
+
+                Console.WriteLine($"{input[(input.Length / 2) - 1]}{input[(input.Length / 2)]}");
+            }
+            else // 7       a s d f g h j
+            {
+                Console.WriteLine(input[(input.Length / 2)]);
+            }
+        }
+    }
+}
