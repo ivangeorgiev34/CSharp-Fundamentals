8ce88882842ddfcb35afd6e1df7ae59223cda345
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:58:49 +0200

    Add exercise 10.TopNumber

---
+++ Methods - Exercise/10.TopNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10.TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            PrintTopNumber(range);
+        }
+        static void PrintTopNumber(int range)
+        {
+            for (int i = 0; i < range; i++)
+            {
+                bool isOddDigit = false;
+                int sum = 0;
+                int currentNumber = i;
+
+                while (currentNumber != 0)
+                {
+                    int digit = currentNumber % 10;
+                    sum = sum + digit;
+                    currentNumber = currentNumber / 10;
+                    if (digit % 2 != 0)
+                    {
+                        isOddDigit = true;
+                    }
+                }
+                if (isOddDigit && sum % 8 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
