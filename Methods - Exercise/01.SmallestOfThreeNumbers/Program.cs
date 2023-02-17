53de491bd8a854be7b2d5410b51d8b9dac997ccf
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-17 07:54:25 +0200

    Add exercise 01.SmallestOfThreeNumbers

---
+++ Methods - Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            Console.WriteLine(SmallestNumber(numberOne, numberTwo, numberThree));
+        }
+        static int SmallestNumber(int numberOne, int numberTwo, int numberThree)
+        {
+            int result = Math.Min(numberOne, Math.Min(numberTwo, numberThree));
+            return result;
+        }
+    }
+}
