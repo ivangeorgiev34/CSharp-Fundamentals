6ebba83377f05971b82821c0d48ea4d0eef9f180
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-18 17:09:08 +0200

    Add exercise 02.ChangeList

---
+++ Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.ChangeList
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> myList = Console.ReadLine().Split().Select(int.Parse).ToList();
+			string command = Console.ReadLine();
+			while (command != "end")
+			{
+				string[] array = command.Split();
+				switch (array[0])
+				{
+					case "Delete":
+						int element = int.Parse(array[1]);
+						myList.RemoveAll(el => el == element);
+						break;
+					case "Insert":
+						myList.Insert(int.Parse(array[2]), int.Parse(array[1]));
+						break;
+					default:
+						break;
+				}
+				command = Console.ReadLine();
+			}
+			Console.WriteLine(string.Join(" ", myList));
+		}
+	}
+}
