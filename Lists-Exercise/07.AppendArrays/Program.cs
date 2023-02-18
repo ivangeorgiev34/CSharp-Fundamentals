dd03c5263eae0463ce7ebaa2736c866c51e83b15
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-18 17:10:48 +0200

    Add exercise 07.AppendArrays

---
+++ Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _07.AppendArrays
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var numbersAsStrings = Console.ReadLine().Split('|').Reverse().ToList();
+
+			var numbers = new List<int>();
+			foreach (var number in numbersAsStrings)
+			{
+				numbers.AddRange(number.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+			}
+
+			Console.WriteLine(string.Join(" ", numbers));
+		}
+	}
+}
