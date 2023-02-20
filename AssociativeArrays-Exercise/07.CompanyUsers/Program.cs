415dc9bae0cceba2a6e2d659f0b0a95772504aeb
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-20 12:11:32 +0200

    Add exercise 07.CompanyUsers

---
+++ AssociativeArrays-Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split(" -> ").ToArray();
+                string companyName = tokens[0];
+                string employeeID = tokens[1];
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+                }
+                if (!companies[companyName].Contains(employeeID))
+                {
+                    companies[companyName].Add(employeeID);
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var company in companies)
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var id in company.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
