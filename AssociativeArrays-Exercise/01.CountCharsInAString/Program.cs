a1b1cdf47615be01d2ce60db8dd68571fb7c975f
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-20 12:09:28 +0200

    Add exercise 01.CountCharsInAString

---
+++ AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] word = input.ToCharArray();
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            foreach (var letter in word)
+            {
+                if (letter != ' ')
+                {
+                    if (!dictionary.ContainsKey(letter))
+                    {
+                        dictionary.Add(letter, 1);
+                    }
+                    else
+                    {
+                        dictionary[letter]++;
+                    }
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
