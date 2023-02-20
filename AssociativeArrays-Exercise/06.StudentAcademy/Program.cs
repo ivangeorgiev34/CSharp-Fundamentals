9ecbb7e6d42c013ee2294ddfdc8a15ad883ab87e
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-20 12:11:13 +0200

    Add exercise 06.StudentAcademy

---
+++ AssociativeArrays-Exercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> grades = new Dictionary<string, List<double>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!grades.ContainsKey(studentName))
+                {
+                    grades.Add(studentName, new List<double>());
+                }
+                grades[studentName].Add(grade);
+            }
+            foreach (var student in grades)
+            {
+                if (student.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+                }
+            }
+        }
+    }
+}
