95fe16ed1e4473bf658bcad463ce137a9279af6d
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-20 12:09:50 +0200

    Add exercise 02.AMinerTask

---
+++ AssociativeArrays-Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            while (true)
+            {
+                string typeOfOre = Console.ReadLine();
+                if (typeOfOre == "stop")
+                {
+                    break;
+                }
+                int numberOfOres = int.Parse(Console.ReadLine());
+                if (!dictionary.ContainsKey(typeOfOre))
+                {
+                    dictionary.Add(typeOfOre, numberOfOres);
+                }
+                else
+                {
+
+                    dictionary[typeOfOre] = dictionary[typeOfOre] + numberOfOres;
+                }
+
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
