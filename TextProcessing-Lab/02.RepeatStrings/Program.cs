98fa8e15664de5dec98a85dff268f5dfc13ec392
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-21 12:04:34 +0200

    Add exercise 02.RepeatStrings

---
+++ TextProcessing-Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            StringBuilder result = new StringBuilder();
+            foreach (var word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result.Append(word);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
