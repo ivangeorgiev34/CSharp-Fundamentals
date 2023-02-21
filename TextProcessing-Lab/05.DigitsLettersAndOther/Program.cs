de99ad80abf0aa0464d1bb892bb51204f2988f36
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-21 12:05:41 +0200

    Add exercise 05.DigitsLettersAndOther

---
+++ TextProcessing-Lab/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] arr = input.ToCharArray();
+            string digits = "";
+            string letters = "";
+            string others = "";
+            foreach (var character in arr)
+            {
+                if (char.IsDigit(character))
+                {
+                    digits = digits + character;
+                }
+                else if (char.IsLetter(character))
+                {
+                    letters = letters + character;
+                }
+                else
+                {
+                    others = others + character;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
