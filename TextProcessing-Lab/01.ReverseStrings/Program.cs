e96a0b4edd5b43b65ebe111e42ff28c5463e0995
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-21 12:04:05 +0200

    Add exercise 01.ReverseStrings

---
+++ TextProcessing-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                list.Add(line);
+            }
+            foreach (var word in list)
+            {
+                string reversedWord = string.Empty;
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord = reversedWord + word[i];
+                }
+                Console.WriteLine($"{word} = {reversedWord}");
+            }
+        }
+    }
+}
