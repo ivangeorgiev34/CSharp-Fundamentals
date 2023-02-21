b44429abd6b97175f8493ec660360de700260d9e
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-21 12:04:55 +0200

    Add exercise 03.Substring

---
+++ TextProcessing-Lab/03.Substring/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string substringToRemove = Console.ReadLine();
+            string str = Console.ReadLine();
+            while (str.Contains(substringToRemove))
+            {
+                str = str.Replace(substringToRemove, "");
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
