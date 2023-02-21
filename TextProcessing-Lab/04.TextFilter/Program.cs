db6a456cda6c89a1e5d26f774235b576fe8972a3
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-21 12:05:21 +0200

    Add exercise 04.TextFilter

---
+++ TextProcessing-Lab/04.TextFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ").ToArray();
+            string text = Console.ReadLine();
+            for (int i = 0; i <= bannedWords.Length - 1; i++)
+            {
+                string asteriks = "";
+                for (int j = 0; j < bannedWords[i].Length - 1; j++)
+                {
+                    asteriks = asteriks + "*";
+                }
+                text = text.Replace(bannedWords[i], new string('*', bannedWords[i].Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
