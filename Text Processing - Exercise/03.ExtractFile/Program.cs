082ca813168c61b9076f294ce7c8dd15f1ae6d9b
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-23 11:23:46 +0200

    Add exercise 03.ExtractFile

---
+++ Text Processing - Exercise/03.ExtractFile/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace _03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            int index = path.LastIndexOf('\\');
+            path = path.Remove(0, index + 1);
+            string[] pathComponents = path.Split('.');
+            Console.WriteLine($"File name: {pathComponents[0]}");
+            Console.WriteLine($"File extension: {pathComponents[1]}");
+        }
+    }
+}
