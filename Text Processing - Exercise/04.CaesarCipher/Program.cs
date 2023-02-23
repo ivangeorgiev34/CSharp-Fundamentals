ddb2dfb34483f801b1c8f9116fc906f5a9f8ee94
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-23 11:24:03 +0200

    Add exercise 04.CaesarCipher

---
+++ Text Processing - Exercise/04.CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace _04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int encryptedtCharNumber = input[i] + 3;
+                char encryptedChar = (char)encryptedtCharNumber;
+                stringBuilder.Append(encryptedChar);
+            }
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
