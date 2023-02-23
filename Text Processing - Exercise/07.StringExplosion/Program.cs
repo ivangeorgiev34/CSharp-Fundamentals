c8b63bef3dff7de044f3879951662e5cbf083de2
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-23 11:24:54 +0200

    Add exercise 07.StringExplosion

---
+++ Text Processing - Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace _07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            int skip = 0;
+
+
+
+            //  abv>1>1>2>2asdasd
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentCharacter = input[i];
+                if (currentCharacter == '>')
+                {
+                    skip = int.Parse(input[i + 1].ToString()) + skip;
+                    sb.Append(currentCharacter);
+                }
+                else if (skip == 0)
+                {
+                    sb.Append(currentCharacter);
+                }
+                else
+                {
+                    skip--;
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
