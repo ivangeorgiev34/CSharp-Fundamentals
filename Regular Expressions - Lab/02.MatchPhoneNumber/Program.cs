b6fdcd15fd15c0c16fc2e12acc49589785550688
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-26 13:41:47 +0200

    Add exercise 02.MatchPhoneNumber

---
+++ Regular Expressions - Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			string pattern = @"\+359([ \-])2\1[0-9]{3}\1[0-9]{4}\b";
+
+			Regex regex = new Regex(pattern);
+
+			MatchCollection matches = regex.Matches(input);
+
+			Console.WriteLine(string.Join(", ", matches));
+		}
+	}
+}
