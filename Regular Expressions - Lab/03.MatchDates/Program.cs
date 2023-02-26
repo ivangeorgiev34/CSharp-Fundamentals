f4f85fedf4561e224bc40eab80c3f3b416b3dac6
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-02-26 13:42:05 +0200

    Add exercise 03.MatchDates

---
+++ Regular Expressions - Lab/03.MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			string pattern = @"(?<day>[0-9]{2})(?<separator>[\-/\.])(?<month>[A-Z][a-z]+)\k<separator>(?<year>[0-9]{4})";
+
+			Regex regex = new Regex(pattern);
+
+			MatchCollection matches = regex.Matches(input);
+
+			foreach (Match match in matches)
+			{
+				Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+			}
+		}
+	}
+}
